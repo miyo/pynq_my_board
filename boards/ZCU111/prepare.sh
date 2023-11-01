997e393aa99bf64b94b403090b427fccb7e66923
Takefumi MIYOHSI <miyo@wasamon.net>
2023-11-01 23:20:20 +0900

    add prepare.sh to gather required files

---
+++ boards/ZCU111/prepare.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+mkdir -p petalinux_bsp/hardware_project
+
+cp ../../fpga_base/ZCU111/myproj/design_1_wrapper.xsa petalinux_bsp/hardware_project
+cp ../../fpga_base/ZCU111/myproj/design_1.tcl .
+cp ../../fpga_base/ZCU111/myproj/project_1.runs/impl_1/design_1_wrapper.bit base.bit
+cp ../../fpga_base/ZCU111/myproj/project_1.gen/sources_1/bd/design_1/hw_handoff/design_1.hwh .
